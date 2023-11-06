9a2723fae6e4440a765071346d3dc0b7c72e9d97
argentdenur <145882006+argentdenur@users.noreply.github.com>
2023-11-05 20:24:58 -0500

    Add files via upload
    turtle making a square and triangle

---
+++ P4LAB1a_MontanezDavin.py
@@ -0,0 +1,14 @@
+import turtle
+print(turtle)           
+wn = turtle.Screen()
+
+# Draw a square
+for i in range(4):
+    turtle.forward(100)
+    turtle.right(90)
+
+
+# Draw a triangle
+for i in range(3):
+    turtle.forward(100)
+    turtle.left(120)
